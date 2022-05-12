c127fd4e967df587d0bf6a9aff97faae4528f0fc
Fabio Lopes <103152758+fabiolopesramos@users.noreply.github.com>
2022-05-12 10:19:27 -0300

    inclusao do projeto gateway

---
+++ src/main/java/com/desafio/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,37 @@
+package com.desafio.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.desafio.gatewayservice.filters.ErrorFilter;
+import com.desafio.gatewayservice.filters.PostFilter;
+import com.desafio.gatewayservice.filters.PreFilter;
+import com.desafio.gatewayservice.filters.RouteFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
